2918a98fa7ec2737a19c478de015c53556a0734b
Haru-Rin <1755214403@qq.com>
2021-10-23 14:53:55 +0800

    first test

---
+++ opencv_lesson1.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread('lena.jpg',0)
+cv2.imshow('lena',img)
+cv2.waitKey(0)
+
+cv2.namedWindow('lena2',cv2.WINDOW_NORMAL)
+cv2.imshow('lena2',img)
+cv2.waitKey(0)
+
+cv2.imwrite('lena_gray.jpg',img)
